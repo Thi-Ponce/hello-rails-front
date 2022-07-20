3c4adc49007706631ef1a8b4f24254bbf667f557
Thi-Ponce <thi.pponce@gmail.com>
2022-07-20 17:59:23 -0300

    install and run linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import greetingsReducer from './reducers/greetings.js';
+import greetingsReducer from './reducers/greetings';
 
 const store = configureStore({ reducer: { greetings: greetingsReducer } });
 export default store;
