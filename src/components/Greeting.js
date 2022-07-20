4623ca6c586b415f14dc27b46538c43dffcf2181
Thi-Ponce <thi.pponce@gmail.com>
2022-07-20 17:45:23 -0300

    create component

---
+++ src/components/Greeting.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDataFromAPI } from '../redux/reducers/greetings';
+
+const Greetings = () => {
+  const selected = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(getDataFromAPI());
+  };
+
+  return (
+    <>
+      Message:
+      {selected}
+      <button type="button" id="messages" onClick={handleClick}>
+        Show a message
+      </button>
+    </>
+  );
+};
+
+export default Greetings;
